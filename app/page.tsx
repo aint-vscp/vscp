a7f6a60f9d70796f3fbb976dff395e556feb2c73
Wat <136457226+aint-vscp@users.noreply.github.com>
2024-01-15 20:26:17 +0800

    a

--- app/page.tsx
+++ app/page.tsx
@@ -22,65 +22,65 @@ export default function Home() {
         <Info />
       </div>
       {/*
-    <div className="flex flex-wrap justify-center min-h-full">
-      <h1 className="p-3">Services</h1>
-      <div className="service-card w-[300px] h-2/3 shadow-xl cursor-pointer snap-start shrink-0 py-6 px-6 bg-white flex flex-col items-start gap-3 transition-all duration-300 group hover:bg-[#202127]">
-        <svg stroke-linejoin="round" stroke-linecap="round" stroke-width="2" stroke="#000000" fill="none" viewBox="0 0 24 24" className="text-5xl h-12 w-12 stroke-gray-800 group-hover:stroke-gray-400" xmlns="http://www.w3.org/2000/svg">
-          <rect ry="2" rx="2" height="14" width="20" y="3" x="2"></rect>
-          <line y2="21" x2="16" y1="21" x1="8"></line>
-          <line y2="21" x2="12" y1="17" x1="12"></line>
-        </svg>
+      <div className="flex flex-wrap justify-center min-h-full">
+        <h1 className="p-3">Services</h1>
+        <div className="service-card w-[300px] h-2/3 shadow-xl cursor-pointer snap-start shrink-0 py-6 px-6 bg-white flex flex-col items-start gap-3 transition-all duration-300 group hover:bg-[#202127]">
+          <svg stroke-linejoin="round" stroke-linecap="round" stroke-width="2" stroke="#000000" fill="none" viewBox="0 0 24 24" className="text-5xl h-12 w-12 stroke-gray-800 group-hover:stroke-gray-400" xmlns="http://www.w3.org/2000/svg">
+            <rect ry="2" rx="2" height="14" width="20" y="3" x="2"></rect>
+            <line y2="21" x2="16" y1="21" x1="8"></line>
+            <line y2="21" x2="12" y1="17" x1="12"></line>
+          </svg>
 
-        <p className="font-bold text-2xl group-hover:text-white text-black/80">
-          WEBSITE SEO
-        </p>
-        <p className="text-gray-400 text-sm">
-          Website ravida surna eveti semen the conse tusio anivite dianne one nivam
-          acestion vue artin dictum.
-        </p>
-        <p className="text-5xl font-bold self-end text-transparent text-stroke">
-          08
-        </p>
-      </div>
+          <p className="font-bold text-2xl group-hover:text-white text-black/80">
+            WEBSITE SEO
+          </p>
+          <p className="text-gray-400 text-sm">
+            Website ravida surna eveti semen the conse tusio anivite dianne one nivam
+            acestion vue artin dictum.
+          </p>
+          <p className="text-5xl font-bold self-end text-transparent text-stroke">
+            08
+          </p>
+        </div>
 
-      <div className="service-card w-[300px] h-1/3 shadow-xl cursor-pointer snap-start shrink-0 py-6 px-6 bg-white flex flex-col items-start gap-3 transition-all duration-300 group hover:bg-[#202127]">
-        <svg stroke-linejoin="round" stroke-linecap="round" stroke-width="2" stroke="#000000" fill="none" viewBox="0 0 24 24" className="text-5xl h-12 w-12 stroke-gray-800 group-hover:stroke-gray-400" xmlns="http://www.w3.org/2000/svg">
-          <rect ry="2" rx="2" height="14" width="20" y="3" x="2"></rect>
-          <line y2="21" x2="16" y1="21" x1="8"></line>
-          <line y2="21" x2="12" y1="17" x1="12"></line>
-        </svg>
+        <div className="service-card w-[300px] h-1/3 shadow-xl cursor-pointer snap-start shrink-0 py-6 px-6 bg-white flex flex-col items-start gap-3 transition-all duration-300 group hover:bg-[#202127]">
+          <svg stroke-linejoin="round" stroke-linecap="round" stroke-width="2" stroke="#000000" fill="none" viewBox="0 0 24 24" className="text-5xl h-12 w-12 stroke-gray-800 group-hover:stroke-gray-400" xmlns="http://www.w3.org/2000/svg">
+            <rect ry="2" rx="2" height="14" width="20" y="3" x="2"></rect>
+            <line y2="21" x2="16" y1="21" x1="8"></line>
+            <line y2="21" x2="12" y1="17" x1="12"></line>
+          </svg>
 
-        <p className="font-bold text-2xl group-hover:text-white text-black/80">
-          WEBSITE SEO
-        </p>
-        <p className="text-gray-400 text-sm">
-          Website ravida surna eveti semen the conse tusio anivite dianne one nivam
-          acestion vue artin dictum.
-        </p>
-        <p className="text-5xl font-bold self-end text-transparent text-stroke">
-          08
-        </p>
-      </div>
+          <p className="font-bold text-2xl group-hover:text-white text-black/80">
+            WEBSITE SEO
+          </p>
+          <p className="text-gray-400 text-sm">
+            Website ravida surna eveti semen the conse tusio anivite dianne one nivam
+            acestion vue artin dictum.
+          </p>
+          <p className="text-5xl font-bold self-end text-transparent text-stroke">
+            08
+          </p>
+        </div>
 
-      <div className="service-card w-[300px] h-1/3 shadow-xl cursor-pointer snap-start shrink-0 py-6 px-6 bg-white flex flex-col items-start gap-3 transition-all duration-300 group hover:bg-[#202127]">
-        <svg stroke-linejoin="round" stroke-linecap="round" stroke-width="2" stroke="#000000" fill="none" viewBox="0 0 24 24" className="text-5xl h-12 w-12 stroke-gray-800 group-hover:stroke-gray-400" xmlns="http://www.w3.org/2000/svg">
-          <rect ry="2" rx="2" height="14" width="20" y="3" x="2"></rect>
-          <line y2="21" x2="16" y1="21" x1="8"></line>
-          <line y2="21" x2="12" y1="17" x1="12"></line>
-        </svg>
+        <div className="service-card w-[300px] h-1/3 shadow-xl cursor-pointer snap-start shrink-0 py-6 px-6 bg-white flex flex-col items-start gap-3 transition-all duration-300 group hover:bg-[#202127]">
+          <svg stroke-linejoin="round" stroke-linecap="round" stroke-width="2" stroke="#000000" fill="none" viewBox="0 0 24 24" className="text-5xl h-12 w-12 stroke-gray-800 group-hover:stroke-gray-400" xmlns="http://www.w3.org/2000/svg">
+            <rect ry="2" rx="2" height="14" width="20" y="3" x="2"></rect>
+            <line y2="21" x2="16" y1="21" x1="8"></line>
+            <line y2="21" x2="12" y1="17" x1="12"></line>
+          </svg>
 
-        <p className="font-bold text-2xl group-hover:text-white text-black/80">
-          WEBSITE SEO
-        </p>
-        <p className="text-gray-400 text-sm">
-          Website ravida surna eveti semen the conse tusio anivite dianne one nivam
-          acestion vue artin dictum.
-        </p>
-        <p className="text-5xl font-bold self-end text-transparent text-stroke">
-          08
-        </p>
-      </div>
-    </div>*/}
+          <p className="font-bold text-2xl group-hover:text-white text-black/80">
+            WEBSITE SEO
+          </p>
+          <p className="text-gray-400 text-sm">
+            Website ravida surna eveti semen the conse tusio anivite dianne one nivam
+            acestion vue artin dictum.
+          </p>
+          <p className="text-5xl font-bold self-end text-transparent text-stroke">
+            08
+          </p>
+        </div>
+      </div>*/}
       <img src='/icon/vscp.ico' className='w-20 h-20' />
       <h1 className="text-6xl font-bold mt-8">Vash is Here</h1>
       <h2 className="text-3xl mt-4">Underconstruction</h2>
